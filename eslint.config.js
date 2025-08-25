2f3e54a217f7d811e66629800a825d313ece89dc
sumire.ogakiuchi <211457108+sumire-ogakiuchi@users.noreply.github.com>
2025-08-25 16:22:32 +0900

    fix: eslint修正

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,10 @@
 // eslint.config.js
 import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
 
 export default [
   js.configs.recommended,
   ...tseslint.configs.recommended,
   {
-    languageOptions: {
-      globals: globals.browser,
-    },
     rules: {
       "no-unused-vars": "warn",
       "no-console": "off",
