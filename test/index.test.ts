34ab2d51dc79d94e71bf72f804902c272e0f5850
sumire.ogakiuchi <211457108+sumire-ogakiuchi@users.noreply.github.com>
2025-08-28 13:15:15 +0900

    fix: テストコードにsetup関数の追加

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,6 +1,9 @@
 import { describe, it, expect } from "vitest";
+import { setup, $fetch } from "@nuxt/test-utils";
 
 describe("トップページのテスト", async () => {
+  await setup();
+
   it("「こんにちは！」表示テスト", async () => {
     const html = await $fetch("/");
 
