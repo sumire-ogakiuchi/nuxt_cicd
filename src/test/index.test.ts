de1c5465417bc22d36fe96b14f1938e115abab5e
sumire.ogakiuchi <211457108+sumire-ogakiuchi@users.noreply.github.com>
2025-08-29 13:17:34 +0900

    fix: vitestプラグインの追加

--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -1,6 +1,6 @@
 import { describe, it, expect } from "vitest";
 import { mount } from "@vue/test-utils";
-import TestPage from "../pages/index.vue";
+import TestPage from "~/pages/index.vue";
 
 describe("トップページのテスト", () => {
   it("「こんにちは！」表示テスト", () => {
