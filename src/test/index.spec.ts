d99940ba86a93709f23080228c4ca252f8507d15
sumire.ogakiuchi <211457108+sumire-ogakiuchi@users.noreply.github.com>
2025-08-29 13:34:42 +0900

    fix: vitestプラグインの追加

--- src/test/index.spec.ts
+++ src/test/index.spec.ts
@@ -1,13 +1,14 @@
 import { describe, it, expect } from "vitest";
 import { setup } from "@nuxt/test-utils";
 import { mount } from "@vue/test-utils";
-import TestPage from "~/pages/index.vue";
 
 describe("トップページのテスト", async () => {
   await setup();
 
-  it("「こんにちは！」表示テスト", () => {
-    const wrapper = mount(TestPage);
+  it("「こんにちは！」表示テスト", async () => {
+    // Nuxtのテスト環境が整ってからパス読み込み
+    const TestPage = await import("~/pages/index.vue");
+    const wrapper = mount(TestPage.default);
 
     expect(wrapper.text()).toContain("こんにちは！");
   });
